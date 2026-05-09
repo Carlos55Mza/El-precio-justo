fd4f3601acfd14d1faed0c1b74dec8511378ef59
Carlos55Mza <gestoriacdz1969@gmail.com>
2026-05-09 11:35:43 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,21 @@
-self.addEventListener('fetch', function(event) {
-  // Este código permite que la app sea instalable
+const cacheName = 'precio-justo-v1';
+const assets = [
+  'index.html',
+  'manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request);
+    })
+  );
 });
